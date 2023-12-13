23bdb6476fb8a3067227ca5ecdafa06a9132deb2
Admin <purohitparthiv@gmail.com>
2023-12-13 23:40:01 +0530

    first commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,11 +4,21 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
-    private String bill;
+    private String bill  = "";
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg)
+        {
+            this.price = 300;
+            bill = bill + "Base Price Of The Pizza: 300" + "/n";
+        }
+        else
+        {
+            this.price = 400;
+            bill = bill + "Base Price Of The Pizza: 400" + "\n";
+        }
     }
 
     public int getPrice(){
@@ -17,18 +27,34 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        this.price = this.price + 80;
+        bill = bill + "Extra Cheese Added = 80"+ "\n";
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg) {
+            this.price = this.price + 70;
+            bill = bill + "Extra Toppings Added = 70" + "\n";
+        }
+        else
+        {
+            this.price = this.price + 120;
+            bill = bill + "Extra Toppings Added = 70" + "\n";
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        this.price  =this.price + 20;
+        bill = bill + "Paperbag Added: 20" + "\n";
     }
 
     public String getBill(){
         // your code goes here
+        bill = bill + "Total Price: " + this.price +"\n";
         return this.bill;
     }
 }
+
